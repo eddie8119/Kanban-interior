7a361f60c2681c881fe1e8768486a0807f0d0ac3
eddie wang <wang8119@gmail.com>
2023-08-07 14:56:10 +0800

    清理

--- src/main.js
+++ src/main.js
@@ -3,10 +3,8 @@ import './tailwind.css'
 import App from './App.vue'
 import router from '@/router/index.js'
 import vuelloStore from './store/index.js'
-import vClickOutside from "click-outside-vue3"
 
 const app = createApp(App)
 app.use(router)
 app.use(vuelloStore)
-app.use(vClickOutside)
 app.mount('#app')
