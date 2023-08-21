d5e18966a1dfb197645c96673b1eae13829d74f5
eddie wang <wang8119@gmail.com>
2023-08-21 17:52:26 +0800

    firebase 配置

---
+++ src/firebase.js
@@ -0,0 +1,70 @@
+import {initializeApp} from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  setDoc,
+  updateDoc,
+  getDoc,
+  getDocs,
+  deleteDoc,
+  doc,
+  query,
+  orderBy,
+  onSnapshot,
+  arrayUnion,
+  arrayRemove
+} from 'firebase/firestore'
+import {
+  getAuth,
+  onAuthStateChanged,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut
+} from "firebase/auth";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDZE5297XjsKrSTqQN8bMecu4SRqG3e1W8",
+  authDomain: "vue-fire-auth-a07e3.firebaseapp.com",
+  projectId: "vue-fire-auth-a07e3",
+  storageBucket: "vue-fire-auth-a07e3.appspot.com",
+  messagingSenderId: "72244899703",
+  appId: "1:72244899703:web:53c290b8844648886b7b72"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const fbAuth = getAuth(firebaseApp)
+const fbDB = getFirestore(firebaseApp)
+
+fbAuth.getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    const unsubscribe = onAuthStateChanged(fbAuth, user => {
+      unsubscribe();
+      resolve(user);
+    }, reject);
+  })
+}
+
+export {
+  fbAuth,
+  fbDB,
+  firebaseApp,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+  addDoc,
+  setDoc,
+  updateDoc,
+  getDoc,
+  getDocs,
+  deleteDoc,
+  doc,
+  collection,
+  query,
+  orderBy,
+  onSnapshot,
+  arrayUnion,
+  arrayRemove
+}
