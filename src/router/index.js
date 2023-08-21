4a1915509d535efd2e13ba5d7b22f988e3ea684d
eddie wang <wang8119@gmail.com>
2023-08-21 16:25:02 +0800

    註冊頁

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,38 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import homePage from '../views/home-page.vue'
+import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
+import ForgotPassword from "../views/ForgotPassword.vue";
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: homePage
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+    meta: {
+      title: "Login",
+    },
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register,
+    meta: {
+      title: "Register",
+    },
+  },
+  {
+    path: "/forgot-password",
+    name: "ForgotPassword",
+    component: ForgotPassword,
+    meta: {
+      title: "Forgot Password",      
+    },
   },
 ]
 
