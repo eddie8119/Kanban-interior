25d4a3acff6db1d37967f54939558d8921948f68
eddie wang <wang8119@gmail.com>
2023-10-08 23:42:42 +0800

    beforeEach 設定

--- src/router/index.js
+++ src/router/index.js
@@ -2,52 +2,66 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import homePage from '../views/home-page.vue'
 import Login from '../views/Login.vue'
 import Register from '../views/Register.vue'
-import ForgotPassword from "../views/ForgotPassword.vue"
+import ForgotPassword from '../views/ForgotPassword.vue'
 import Profile from '../views/Profile.vue'
+// import { getAuth } from 'firebase/auth'
+import { fbAuth } from '../firebase/firebase'
+// import { getUidToken } from '../apis/auth'
+// import store from '@/store'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: homePage
+    component: homePage,
   },
   {
-    path: "/login",
-    name: "Login",
+    path: '/login',
+    name: 'Login',
     component: Login,
     meta: {
-      title: "Login",
+      title: 'Login',
     },
   },
   {
-    path: "/register",
-    name: "Register",
+    path: '/register',
+    name: 'Register',
     component: Register,
     meta: {
-      title: "Register",
+      title: 'Register',
     },
   },
   {
-    path: "/forgot-password",
-    name: "ForgotPassword",
+    path: '/forgot-password',
+    name: 'ForgotPassword',
     component: ForgotPassword,
     meta: {
-      title: "Forgot Password",      
+      title: 'Forgot Password',
     },
   },
   {
-    path: "/profile",
-    name: "Profile",
+    path: '/profile',
+    name: 'Profile',
     component: Profile,
     meta: {
-      title: "profile",      
+      title: 'profile',
     },
   },
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
+  routes,
+})
+
+router.beforeEach((to, next) => {
+  // console.log('fbAuth.currentUser', fbAuth.currentUser.uid)
+  // const checkLoginValue = router.app.$store.getters['user/getCheckLogin']
+  // const checkLoginValue = router.app
+  // console.log(checkLoginValue)
+  if (to.name === 'login' && fbAuth.currentUser.uid) {
+    return { name: 'home' }
+  }
 })
 
 export default router
